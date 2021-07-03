f7799adf2f1651c0813daf773d709f41874ad364
Rima Jana <66671637+rimajana@users.noreply.github.com>
2021-07-03 15:32:13 +0530

    Update game.js

--- game.js
+++ game.js
@@ -63,7 +63,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('/end_page.html');
+        return window.location.assign('end_page.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
